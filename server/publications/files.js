d65edd0f925c75566edfd21e1d63f6493a409230
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-24 17:37:43 +0300

    Add singleFile publication

--- server/publications/files.js
+++ server/publications/files.js
@@ -1,3 +1,8 @@
 Meteor.publish("allFiles", function () {
   return Files.find();
 });
+
+Meteor.publish("singleFile", function (fileId) {
+  // Return a single file, by ID
+  return Files.find(fileId);
+});
