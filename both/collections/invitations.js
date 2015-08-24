87e1cc69ef38a7e08883f91aedfd39ca801ae47b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-24 17:45:56 +0300

    Add Invitations Schema

--- both/collections/invitations.js
+++ both/collections/invitations.js
@@ -1,1 +1,19 @@
 Invitations = new Mongo.Collection("invitations");
+
+InvitationsSchema = new SimpleSchema({
+  emails: {
+    type: [String],
+    label: "E-mail address(es)",
+    regex: SimpleSchema.RegEx.Email
+  },
+  file: {
+    type: String,
+    autoform: {
+      afFieldInput: {
+        type: "fileUpload",
+        collection: "Files",
+        label: "Choose a file"
+      }
+    }
+  }
+});
