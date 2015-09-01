6d832fd7470a3205a393971d9419796fd3c4d38a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-01 14:53:47 +0300

    Add invitationFiles publication

--- server/publications/files.js
+++ server/publications/files.js
@@ -6,3 +6,8 @@ Meteor.publish("singleFile", function (fileId) {
   // Return a single file, by ID
   return Files.find(fileId);
 });
+
+Meteor.publish("invitationFiles", function (fileIdArray) {
+  // Return files found in file ID array
+  return Files.find({_id: {$in: fileIdArray}});
+});
