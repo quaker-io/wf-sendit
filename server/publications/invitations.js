255b32aa9c28a1b983e26823c62c31af57361aea
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-07 10:19:52 +0300

    Add invitationComposite publication

--- server/publications/invitations.js
+++ server/publications/invitations.js
@@ -6,3 +6,27 @@ Meteor.publish("singleInvitation", function (invitationId) {
   // Return a single Invitation, by ID
   return Invitations.find(invitationId);
 });
+
+Meteor.publishComposite('invitationComposite', function (invitationId) {
+  return {
+    find: function () {
+      console.log(invitationId);
+      // Get specific invitation by ID
+      var invitations = Invitations.find(invitationId);
+      console.log(invitations.count());
+      // Find a specific invitation
+      return invitations;
+    },
+    children: [
+      {
+        find: function (invitation) {
+          // Get all file IDs for invitation
+          var fileIds = invitation.fileIds;
+
+          // Find all files related to invitation
+          return Files.find({ _id: { $in: fileIds }})
+        }
+      }
+    ]
+  }
+});
