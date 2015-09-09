f3b8c0b7e5c5e37b4e49358c263f9eae81762566
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-09 21:41:40 +0300

    Add sendInvitationEmail method; add Mailgun

--- server/methods.js
+++ server/methods.js
@@ -1,16 +1,58 @@
 Meteor.methods({
-  sendEmail: function (to, from, subject, text) {
+  'sendEmail': function (to, from, subject, text) {
     check([to, from, subject, text], [String]);
 
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
     this.unblock();
 
-    Email.send({
+    // Construct a message object
+    var messageObject = {
       to: to,
       from: from,
       subject: subject,
       text: text
-    });
+    };
+
+    Meteor.Mailgun.send(messageObject);
+  },
+  'sendInvitationEmail': function (invitationId) {
+    check([invitationId], [String]);
+    this.unblock();
+
+    // Get the application absolute URL
+    var absoluteUrl = Meteor.absoluteUrl();
+
+    // Get the application base url, without trailing slash
+    var baseUrl;
+
+    if (absolute.substr(-1) === '/') {
+        baseUrl = absoluteUrl.substr(0, str.length - 1);
+    } else {
+      baseUrl = absoluteUrl;
+    }
+
+    // Construct an invitation URL
+    var invitationUrl = baseUrl + "/invitation/" + invitationId;
+
+    // Construct a from address
+    var from = 'contact@' + baseUrl;
+
+    // Create a basic message text
+    var message = "You have an invitation at " + invitationUrl;
+
+    // Get invitation
+    var invitation = Invitations.findOne(invitationId);
+
+    // Get invitation title
+    var title = invitation.title;
+
+    // Get recipient email addresses
+    var recipients = invitation.emails;
+
+    for (index in recipients) {
+      // Send email to recipient
+      Meteor.call('sendEmail', recipients[index], from, title, message);
+    };
   }
 });
