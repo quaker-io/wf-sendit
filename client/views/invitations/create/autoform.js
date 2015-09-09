ab1445cb1cd7539370d8342aea4e6810240a18ec
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-09 21:40:40 +0300

    Call sendInvitationEmail with invitationId

--- client/views/invitations/create/autoform.js
+++ client/views/invitations/create/autoform.js
@@ -1,5 +1,8 @@
 AutoForm.addHooks("createInvitationForm", {
   onSuccess: function (formType, invitationId) {
+    // Send the invitation emails
+    Meteor.call('sendInvitationEmail', invitationId);
+
     // Show the invitation page for the created invitation
     Router.go("viewSingleInvitation", {_id: invitationId});
   }
