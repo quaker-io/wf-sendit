e0c900e0610c59144e09352dfeca2a27332b1af9
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-11-04 18:54:55 +0200

    Add PrettyEmail options

---
+++ server/config/prettyEmail.js
@@ -0,0 +1,10 @@
+PrettyEmail.options = {
+  from: 'editor@westernfriend.org',
+  logoUrl: 'https://westernfriend.org/sites/default/files/WesternFriend-TinCanWomanWired-75x100_0.png',
+  companyName: 'Western Friend',
+  companyUrl: 'https://westernfriend.org',
+//  companyAddress: '123 Street, ZipCode, City, Country',
+//  companyTelephone: '+1234567890',
+  companyEmail: 'editor@westernfriend.org,
+  siteName: 'WF-SendIt'
+};
