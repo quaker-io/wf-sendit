c2b791260b9e45019ba31974fd9467745acc1b24
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-11-18 19:57:51 +0200

    Add settings subscription

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -1,3 +1,11 @@
+Template.settings.created = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Subscribe to setings
+  instance.subscribe("settings");
+};
+
 Template.settings.helpers({
   settings: function () {
     var settings = Settings.findOne();
