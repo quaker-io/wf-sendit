02c71b7a0663b1398dc20fd05c049850c8a20e0b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-23 14:36:55 +0200

    Include link URL

--- server/invitationEmail/invitationEmail.js
+++ server/invitationEmail/invitationEmail.js
@@ -2,6 +2,8 @@ Meteor.startup(function () {
   var emailTemplate = `
   <h2>{{ title }}</h2>
   <p>{{ message }}</p>
+  <p>{{ linkText }}</p>
+  <a href="{{ url }}">{{ url }}</a>
   `
 
   SSR.compileTemplate('invitationEmail', emailTemplate);
