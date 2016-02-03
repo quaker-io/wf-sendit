7eee90417bc3a5df5b17799e2c4dcbf1ee532ca2
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-03 20:00:26 +0200

    Protect from traceback with checks

--- server/methods/logo.js
+++ server/methods/logo.js
@@ -2,15 +2,17 @@ Meteor.methods({
   getLogoUrl: function () {
     var settings = Settings.findOne();
 
-    // Get logo ID
-    var logoId = settings.brandingImageId;
+    if (settings && settings.brandingImageId) {
+      // Get logo ID
+      var logoId = settings.brandingImageId;
 
-    // Get logo
-    var logo = Files.findOne(logoId);
+      // Get logo
+      var logo = Files.findOne(logoId);
 
-    // Get relative URL for logo
-    var logoUrl = logo.url();
+      // Get relative URL for logo
+      var logoUrl = logo.url();
 
-    return logoUrl;
+      return logoUrl;
+    }
   }
 });
